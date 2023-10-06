ef617a24fe7ae2cb083efe83190d9b419c4873c4
Aidana Ualikhan <aidana.ualikhan35@gmail.com>
2023-10-06 17:54:16 +0600

    added grid and fixed scroll

--- Titan/Assets/Scripts/JournalManager.cs
+++ Titan/Assets/Scripts/JournalManager.cs
@@ -6,22 +6,23 @@ using UnityEngine.UI;
 
 public class JournalManager : MonoBehaviour
 {
-    public TextMeshProUGUI journalText;
     public List<DisasterEvent> disasterEvents = new List<DisasterEvent>();
 
     public GameObject objectBlockPrefab; // Reference to your object block prefab
     public Transform panelContent; // Reference to the content transform of the panel
     public float spacing = 10f; // Specify the spacing between objects
 
+    public Transform selectedDetail;
+
     void Start()
     {
-        if (disasterEvents.Count > 0)
+        /*if (disasterEvents.Count > 0)
         {
             foreach (var disasterEvent in disasterEvents)
             {
                 Debug.Log(disasterEvent.disasterName);
             }
-        }
+        }*/
 
         PopulateObjectList();
     }
@@ -38,6 +39,9 @@ public class JournalManager : MonoBehaviour
             objectBlock.GetComponentInChildren<TextMeshProUGUI>().text = disasterEvent.disasterName;
             // Set other UI elements with disasterEvent properties as needed
 
+            Button button = objectBlock.GetComponent<Button>();
+            button.onClick.AddListener(() => changeDisplayedInfo(disasterEvent));
+
             // Adjust the position of the objectBlock prefab
             RectTransform rt = objectBlock.GetComponent<RectTransform>();
             rt.anchoredPosition = new Vector2(rt.anchoredPosition.x, -yOffset); // Set the y position
@@ -45,12 +49,18 @@ public class JournalManager : MonoBehaviour
         }
     }
 
-    public void ShowObjectDescription(DisasterEvent selectedEvent)
+    void changeDisplayedInfo(DisasterEvent disasterEvent)
     {
-        if(journalText != null)
-        {
-            journalText.text = selectedEvent.disasterDescription;
-        }
+
+        // Debug.Log("You have clicked the button!");
+
+        GameObject infoPanel = selectedDetail.GetChild(0).gameObject;
+        var texts = infoPanel.GetComponentsInChildren<TextMeshProUGUI>();
+        texts[0].text = disasterEvent.disasterName;
+        texts[1].text = "Probability: " + disasterEvent.disasterProbability.ToString();
+        texts[2].text = "Intensity: " + disasterEvent.disasterIntensity.ToString();
+        texts[3].text = disasterEvent.disasterDescription;
+
     }
 }
 
